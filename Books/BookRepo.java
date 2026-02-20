7effa3341ceb4aa432eff564df8ba3685208d013
harsh-799 <harshanand7898@gmail.com>
2026-02-20 18:02:48 +0530

    LMS/Book: BookRepo [Heart of Project] - includes serialization and deserialization stuffs

---
+++ Books/BookRepo.java
@@ -0,0 +1,39 @@
+package Books;
+
+import java.io.*;
+import java.util.ArrayList;
+
+public class BookRepo {
+
+    private static final String FILE_PATH = "Books/BooksData.ser";
+
+    public void serializeData(ArrayList<Book> bookList){
+        try {
+            FileOutputStream fos = new FileOutputStream(FILE_PATH);
+            ObjectOutputStream oos = new ObjectOutputStream(fos);
+
+            oos.writeObject(bookList);
+
+            oos.close();
+            fos.close();
+        } catch (IOException e) {
+            System.out.println("Error while saving book data.");
+        }
+    }
+
+    public ArrayList<Book> deserializeData(){
+        try {
+            FileInputStream fis = new FileInputStream(FILE_PATH);
+            ObjectInputStream ois = new ObjectInputStream(fis);
+
+            ArrayList<Book> bookList = (ArrayList<Book>) ois.readObject();
+
+            ois.close();
+            fis.close();
+            return bookList;
+
+        }catch (IOException | ClassNotFoundException e) {
+            return new ArrayList<>();
+        }
+    }
+}
