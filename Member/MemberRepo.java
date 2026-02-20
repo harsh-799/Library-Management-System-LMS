9f54f7e7e387cf8fbbdf353d8a50f87bec8469ce
harsh-799 <harshanand7898@gmail.com>
2026-02-20 18:04:38 +0530

    LMS/Book: MemberRepo [Heart of Project] - includes serialization and deserialization stuffs

---
+++ Member/MemberRepo.java
@@ -0,0 +1,38 @@
+package Member;
+
+import java.io.*;
+import java.util.ArrayList;
+
+public class MemberRepo {
+
+    private static final String FILE_PATH = "Member/MemberData.ser";
+
+    public void serialize(ArrayList<Member> members){
+        try {
+            FileOutputStream fos = new FileOutputStream(FILE_PATH);
+            ObjectOutputStream oos = new ObjectOutputStream(fos);
+
+            oos.writeObject(members);
+            oos.close();
+            fos.close();
+        }catch (IOException e){
+            System.out.println("Error while saving the data");
+        }
+    }
+
+    public ArrayList<Member> deserialize(){
+        ArrayList<Member> members = new ArrayList<>();
+
+        try {
+            FileInputStream fis = new FileInputStream(FILE_PATH);
+            ObjectInputStream ois = new ObjectInputStream(fis);
+            members = (ArrayList<Member>) ois.readObject();
+
+             ois.close();
+             fis.close();
+        } catch (IOException  | ClassNotFoundException e){
+        }
+
+        return members;
+    }
+}
