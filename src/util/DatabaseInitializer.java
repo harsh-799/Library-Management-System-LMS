ca2e8ba47b9a952f68ffebc56a7d5e9fc40f1575
harsh-799 <harshanand7898@gmail.com>
2026-03-14 17:00:18 +0530

    LMS: Automatic Table Initializer for Smooth Workflow
    - creates the required tables if it doesn't exists so that users don't have burden to create on there own in MySQL CLI/Workbench

---
+++ src/util/DatabaseInitializer.java
@@ -0,0 +1,36 @@
+package util;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class DatabaseInitializer {
+
+    public void initializeAllTables(){
+        try(Connection conn = DatabaseConnection.connectDB();
+            Statement stmt = conn.createStatement()){
+
+            stmt.executeUpdate("CREATE TABLE IF NOT EXISTS " +
+                    "admin_table (Username VARCHAR(50) PRIMARY KEY, Password VARCHAR(255))");
+
+            stmt.executeUpdate("CREATE TABLE IF NOT EXISTS " +
+                    "member_cred_table (Member_ID BIGINT PRIMARY KEY, Username VARCHAR(10) UNIQUE, " +
+                    "Password VARCHAR(255))");
+
+            stmt.executeUpdate("CREATE TABLE IF NOT EXISTS " +
+                    "member_table (Member_ID BIGINT PRIMARY KEY, Fullname VARCHAR(20))");
+
+            stmt.executeUpdate("CREATE TABLE IF NOT EXISTS " +
+                    "book_table (Book_ID INT PRIMARY KEY, Title VARCHAR(20), Author VARCHAR(20), AVAIL_QTY INT," +
+                    "TOTAL_QTY INT)");
+
+            stmt.executeUpdate("CREATE TABLE IF NOT EXISTS " +
+                    "book_issued (Issue_ID BIGINT PRIMARY KEY, Member_ID BIGINT, Book_ID INT, Issued_Date DATE, " +
+                    "Return_Date DATE, FOREIGN KEY(Member_ID) REFERENCES member_table(Member_ID), " +
+                    "FOREIGN KEY(Book_ID) REFERENCES book_table(Book_ID))");
+
+        }catch (SQLException e){
+            System.out.println("Database Initializer error" + e);
+        }
+    }
+}
